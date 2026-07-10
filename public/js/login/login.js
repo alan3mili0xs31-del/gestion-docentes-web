0eb7e586e147c393408d74c4a519889bec433e28
alan-olaya-toscano <alan3mili0xs31@gmail.com>
2026-07-10 00:31:34 -0500

    actualizacion de los enlaces a los html, js y css

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -4,17 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     formLogin.addEventListener('submit', (e) => {
         e.preventDefault();
-        
+
         const cedula = document.getElementById('cedula').value;
         const password = document.getElementById('password').value;
         const alertaError = document.getElementById('alertaError');
-        
+
         const docentes = JSON.parse(localStorage.getItem('docentes')) || [];
         const docente = {id: 1}// docentes.find(d => d.cedula === cedula && d.password === password);
-        
+
         if (docente) {
             sessionStorage.setItem('docenteLogueadoId', docente.id);
-            window.location.href = '../html/home.html';
+            window.location.href = '../views/home.html';
         } else {
             alertaError.classList.remove('d-none');
         }
